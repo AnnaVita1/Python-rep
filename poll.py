6072878ffa158eaa7fa6b66660feba308bf3b88d
AnnaVi <116361142+AnnaVita1@users.noreply.github.com>
2022-12-13 18:25:15 +0300

    Updating code

--- poll.py
+++ poll.py
@@ -1,18 +1,14 @@
 responses = {}
-
 polling_active = True
-
 while polling_active:
-  name = input("\nWhat is your name? ")
-  response = input("What do you want to do now? ")
-  responses[name] = response
-  print("Cool!")
-  
-  repeat = input("Would you like to let another person respond? (yes/no) ")
-  if repeat == 'no':
-    polling_active = False
-  
-  
-  print ("\n----Poll Results----")
-  for name, response in responses.items():
-    print(f"{name} is really wants to {response}.")
+    name = input("\nWhat is your name? ")
+    response = input("What do you want to do now? ")
+    responses[name] = response
+    print("Cool!")
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == 'no':
+        print("\n----Poll Results----")
+        for name, response in responses.items():
+            print(f"{name} is really wants to {response}.")
+        polling_active = False
+        break
